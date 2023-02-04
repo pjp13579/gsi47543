f88d1323338e5dfa3bf476c52ddc693de6693232
pjp13579 <tisppereira@gmail.com>
2023-02-04 18:23:10 +0000

    fixed user dto, basic swagger documentation

---
+++ DTO/ClassMapper.cs
@@ -0,0 +1,55 @@
+﻿namespace DTO
+{
+    public static class ClassMapper
+    {
+        // public static K generic(T obj, K dto)
+        // {
+        //     MapperConfiguration config = new MapperConfiguration(conf => conf.CreateMap(typeof(T), typeof(K)));
+        //     var mapper = new Mapper(config);
+        // 
+        //     return mapper.Map<K>(obj);
+        // }
+
+        public static UserDTO convertForDTO(GSI47543.DB.User user)
+        {
+            UserDTO mappedUser = new UserDTO();
+            mappedUser.ID = user.ID;
+            mappedUser.displayName = user.displayName;
+            mappedUser.ou = user.ou;
+            mappedUser.accountActive = user.accountActive;
+            mappedUser.userAccountControl = user.userAccountControl;
+            mappedUser.logonCount = user.logonCount;
+            mappedUser.pwdExpires = user.pwdExpires.HasValue ? user.pwdExpires.Value.ToString("dd-MM-yyyy HH:mm:ss"): null;
+            mappedUser.whenCreated = user.whenCreated.HasValue ? user.whenCreated.Value.ToString("dd-MM-yyyy HH:mm:ss") : null;
+            mappedUser.pwdLastSet = user.pwdLastSet.HasValue ? user.pwdLastSet.Value.ToString("dd-MM-yyyy HH:mm:ss") : null;
+            return mappedUser;
+        }
+
+        public static GSI47543.DB.User convertForUpdate(GSI47543.AD.User user)
+        {
+            GSI47543.DB.User use = new GSI47543.DB.User();
+
+            use.ID = user.objectGUID.ToString();
+            use.pwdLastSet = user.pwdLastSet;
+            use.logonCount = user.logonCount;
+            use.displayName = user.displayName;
+
+            // distinguishedName is composed of several fields, we only want the OU
+            string ou = user.distinguishedName.Split(",").ToList().FirstOrDefault(x => x.StartsWith("OU="));
+            //some users do not belong to OU, if OU are on the 2º field. If º field is not OU, user does not belong to OU, therefore not adding to database(db saves null value for OU)
+            use.ou = string.IsNullOrEmpty(ou) ? null : ou.Split("=")[1];
+
+            use.whenCreated = user.whenCreated;
+            use.pwdExpires = user.pwdExpires;
+            use.userAccountControl = user.userAccountControl;
+
+            // check if flag 0x0002 (account disabled) is in userAccountControl
+            if (Convert.ToBoolean(user.userAccountControl & 0x0002))
+                use.accountActive = false;
+            else
+                use.accountActive = true;
+
+            return use;
+        }
+    }
+}
---
+++ GSI47543/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Configuring Swagger/OpenAPI: https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    // Version Information
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = "WebAPI AD Manager",
+        Description = "Update users and manage their information",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Contacts",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    // Endpoints documentation
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
